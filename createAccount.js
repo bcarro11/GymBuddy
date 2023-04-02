48b504da9dc1030a736fdb2d884c4c43fa175294
Brenden <brendenwc@gmail.com>
2023-04-02 15:11:13 -0400

    Login & Acc Creation HTML/CSS/JS

---
+++ createAccount.js
@@ -0,0 +1,13 @@
+/**
+ * GROUP: Gym Buddy
+ * MEMBERS: ​Brenden Carroll, Stefani Page, Elina Tsykhmistrenko, Justin White, Hamza Zgidou​ 
+ * COURSE: CMSC 495:7383
+ * FILE: createAccount.js
+ * DATE: April 4, 2023
+ */
+
+const cnclCreate = document.getElementById("cancelCreateBtn").addEventListener("click", goHome);
+
+function goHome(){
+    window.location.href="login.html";
+}
